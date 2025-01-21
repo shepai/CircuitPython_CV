40c8d4a46cf42efccbe427530826b1c859675c68
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-01-21 10:28:18 +0000

    saving images

---
+++ examples/save_img.py
@@ -0,0 +1,26 @@
+import storage
+import os
+
+# Example data to save
+data = "Hello, CircuitPython! This is a test file."
+
+# Ensure the storage is writable
+storage.remount("/", readonly=False)
+
+def save_image(filename, buf):
+
+    # Write the raw byte array to a file
+    try:
+        with open(filename, "wb") as file:
+            file.write(buf)
+        print(f"Image saved as '{filename}' successfully.")
+    except OSError as e:
+        print(f"Failed to save image: {e}")
+        
+from test import AI_cam
+from ov7670 import camera
+
+cam=AI_cam(camera)
+a,buf=cam.takePic()
+
+save_image("/image.raw", buf)
